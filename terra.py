79820c981290719f7c2e3a61a76b2141047594c3
Victor <victor.lopez@king.com>
2023-09-04 20:00:47 +0200

    UI, and adjustments

--- terra.py
+++ terra.py
@@ -14,7 +14,7 @@ scale_factor = 0.01
 
 earth_radius_game = 63710.00 * scale_factor
 rocket_height_game = 2.00 * scale_factor
-rocket_mass_game = 3050 * scale_factor
+rocket_mass_game = 305000 * scale_factor
 
 
 # Define the real-world distances for the atmospheric layers
@@ -122,17 +122,92 @@ class Earth:
 
 class Rocket:
     def __init__(self, body):
-        self.entity = Entity(parent=earth.entity, model='soyuz-fg.glb', scale_y=rocket_height_game, scale_x=rocket_height_game, scale_z=rocket_height_game)
-        self.entity.scale *= 0.01
+        self.entity = Entity(parent=earth.entity, scale_y=rocket_height_game, scale_x=rocket_height_game, scale_z=rocket_height_game)
+
+        self.rocket = Entity(parent=self.entity, model='soyuz-fg.glb', scale_y=1, scale_x=1, scale_z=1)
+
+        self.rocket.scale *= 1 * scale_factor
+
         self.entity.world_position = earth.entity.world_position + Vec3(0, earth_radius_game + rocket_height_game / 2, 0)
         self.rocket_body = body
         self.gimbal_rotation_angle = 0.0
         self.pitch_angle = 0.0
         self.yaw_angle = 0.0
 
-        self.gimbal = Entity(parent=self.entity, model='cube', color=color.red, scale=(0.1, 0.1, 0.5))
+        self.gimbal = Entity(parent=self.entity, model='cube', color=color.red, scale=(0.01, 0.01, 0.05))
         self.gimbal_position_offset = Vec3(0, -rocket_height_game / 2, 0)
         self.gimbal.world_position = self.entity.world_position + self.gimbal_position_offset
+
+        # Defining stage masses
+        self.first_stage_mass = rocket_mass_game * 0.4  # Example mass distribution
+        self.second_stage_mass = rocket_mass_game * 0.35
+        self.third_stage_mass = rocket_mass_game * 0.25
+        
+        # Starting with all stages attached
+        self.current_mass = rocket_mass_game
+        
+        scale = 10 * scale_factor
+
+        # Representing stages with simple shapes (e.g., cylinders)
+        # Set the parent at the time of creation.
+        self.first_stage = Entity(parent=self.entity,
+                                model='cube', 
+                                scale=(scale, scale, scale), 
+                                color=color.red)
+
+        # Set the parent at the time of creation.
+        self.second_stage = Entity(parent=self.entity,
+                                model='cube', 
+                                scale=(scale, scale, scale), 
+                                color=color.green)
+
+        # Position the second_stage just above the top of self.entity. 
+        # If the height of self.entity is based on its y-scale, then:
+        self.second_stage.y = (self.entity.scale.y / 4 / self.rocket.scale.y) * 0.4
+
+
+        # Set the parent at the time of creation.
+        # self.third_stage = Entity(parent=self.entity,
+        #                         model='cube', 
+        #                         scale=(scale, scale, scale), 
+        #                         color=color.cyan)
+
+        # Position the second_stage just above the top of self.entity. 
+        # If the height of self.entity is based on its y-scale, then:
+        #self.third_stage.y = (self.entity.scale.y / 4 / self.rocket.scale.y) * 0.75
+
+
+        self.rocket_body = body
+        self.gimbal_rotation_angle = 0.0
+        self.pitch_angle = 0.0
+        self.yaw_angle = 0.0
+
+
+        self.initial_position = self.entity.world_position
+        self.previous_position = self.entity.world_position
+
+        self.traveled_distance = 0
+        self.current_fuel_mass = initial_fuel_mass
+
+        self.current_stage = 1  # Starting stage
+
+        self.frame_counter = 0  # Counting frames since the last UI update
+
+    def jettison_first_stage(self):
+        if self.first_stage:
+            self.first_stage.disable()  # Assuming a method to make the entity inactive or remove it
+            self.current_mass -= self.first_stage_mass
+            self._update_mass()
+            self.first_stage = None  # Remove reference so we can't jettison it again
+            self.current_stage = 2  # Move to the second stage
+
+    def jettison_second_stage(self):
+        if self.second_stage:
+            self.second_stage.disable()  # Remove or disable the visual representation
+            self.current_mass -= self.second_stage_mass
+            self._update_mass()
+            self.second_stage = None  # Remove reference
+            self.current_stage = 3  # Move to the third stage
 
 
     def update(self):
@@ -142,6 +217,49 @@ class Rocket:
         self._consume_fuel_and_apply_thrust()
         self._apply_drag_force()
         self._update_position()
+
+        self.frame_counter += 1
+
+        self.traveled_distance = distance(self.initial_position, self.entity.world_position)
+        # Update the UI elements every 10 frames (or whatever number you prefer)
+        if self.frame_counter % 6 == 0:
+            altitude = self.entity.world_position.y - earth_radius_game
+            
+            # Determine the current atmospheric layer
+            if altitude <= 120:
+                atmospheric_layer = "Troposphere"
+            elif altitude <= 500:
+                atmospheric_layer = "Stratosphere"
+            elif altitude <= 850:
+                atmospheric_layer = "Mesosphere"
+            elif altitude <= 6000:
+                atmospheric_layer = "Thermosphere"
+            else:
+                atmospheric_layer = "Exosphere"
+
+            velocity = p.getBaseVelocity(self.rocket_body)[0]  # Assuming this returns a velocity vector
+
+            speed = math.sqrt(velocity[0]**2 + velocity[1]**2 + velocity[2]**2) # Calculate the magnitude of the velocity vector
+            self.previous_position = self.entity.world_position  # Store the current position for the next frame
+
+            speed_label.text = f"SPEED: {speed:.2f} m/s" 
+            atmospheric_layer_label.text = f"LAYER: {atmospheric_layer}"
+
+
+            altitude = self.entity.world_position.y  # Assuming y is the altitude (adjust accordingly)
+
+            fuel_percentage = (self.current_fuel_mass / initial_fuel_mass) * 100  # Calculate the percentage of fuel remaining
+
+            altitude_label.text = f"ALTITUDE: {altitude:.2f}"  # Rounded to two decimal places for display
+            distance_label.text = f"DISTANCE: {self.traveled_distance:.2f}"  # Rounded to two decimal places for display
+            fuel_label.text = f"FUEL: {fuel_percentage:.2f}%"  # Display the fuel percentage
+        
+            # To calculate the speed, we need the change in position divided by time.
+            current_stage_label.text = f"STAGE: {self.current_stage}"
+
+            info = p.getDynamicsInfo(self.rocket_body, -1)
+            current_mass = info[0] / scale_factor # Mass is the first item in the tuple returned by getDynamicsInfo
+            mass_label.text = f"MASS: {current_mass:.2f} kg"
 
     def _adjust_gimbal_rotation(self):
         global held_keys
@@ -154,6 +272,13 @@ class Rocket:
             self.pitch_angle += 1.0
         if held_keys['down arrow']:
             self.pitch_angle -= 1.0
+
+        # Jettison stages with number keys
+        if held_keys['1']:
+            self.jettison_first_stage()
+        if held_keys['2']:
+            self.jettison_second_stage()
+
 
         # Limit gimbal rotation angles within a range
         self.pitch_angle = clamp(self.pitch_angle, -45.0, 45.0)
@@ -182,17 +307,20 @@ class Rocket:
         # Set the new gravity
         p.setGravity(0, -new_gravity, 0)
 
+
+    def _update_mass(self):
+        # Calculate the current total mass of the rocket (initial mass + remaining fuel)
+        current_total_mass = (self.current_mass * 0.2) + (m / initial_fuel_mass) * (rocket_mass_game)
+
+        # Update the bullet body mass
+        p.changeDynamics(self.rocket_body, -1, mass=current_total_mass, linearDamping=0.0, angularDamping=0.0, maxJointVelocity = 7500)
+
     def _consume_fuel_and_apply_thrust(self):
-        global m, thrust_force, held_keys, button, oxygen_level, initial_fuel_mass
+        global m, thrust_force, held_keys, oxygen_level, initial_fuel_mass
 
         fuel_consumed_per_update = mass_flow_rate * time.dt
         fuel_fraction_left = m / (V * density_RP1)  # fraction of initial fuel left
 
-        # Calculate the current total mass of the rocket (initial mass + remaining fuel)
-        current_total_mass = (rocket_mass_game * 0.2) + (m / initial_fuel_mass) * (rocket_mass_game)
-
-        # Update the bullet body mass
-        p.changeDynamics(self.rocket_body, -1, mass=current_total_mass)
 
         # thrust_force_vector = Vec3(
         #     self.thrust_direction.x * thrust_force,
@@ -205,7 +333,7 @@ class Rocket:
                                     0
         )
         
-        if m > 0 and (held_keys['space'] or (button.hovered and held_keys['left mouse'])):
+        if m > 0 and held_keys['space']:
             # Apply thrust force to the rocket's physics body
             p.applyExternalForce(self.rocket_body, -1, thrust_force_vector, self.gimbal.world_position, p.LINK_FRAME)
 
@@ -214,6 +342,10 @@ class Rocket:
 
             # Reduce remaining fuel
             m -= fuel_consumed_per_update
+            self.current_fuel_mass -= fuel_consumed_per_update  # Decrease current fuel mass
+
+        self._update_mass()
+
 
     def _apply_drag_force(self):
         # Calculate drag force
@@ -247,11 +379,11 @@ class GameCamera:
         self.is_rocket_state = True  # default to rocket state
 
     def update(self):
-        if held_keys['1']:
+        if held_keys['o']:
             self.entity.fov = 10
             self.is_rocket_state = True
 
-        if held_keys['2']:
+        if held_keys['p']:
             self.entity.fov = 60
             self.is_rocket_state = False
 
@@ -276,12 +408,19 @@ class GameCamera:
 
 
 def initialize_ui():
-    global button, oxygen_bar, oxygen_level
+    global oxygen_bar, oxygen_level, altitude_label, distance_label, fuel_label, speed_label, current_stage_label, atmospheric_layer_label, mass_label
 
-    button = Button(text='Combust', color=color.green, scale=(0.1, 0.05), position=(-0.6, 0.4))
-    
     oxygen_bar = Entity(parent=camera.ui, model='quad', color=color.black, scale=(0.2, 0.05), position=(-0.6, -0.4))
     oxygen_level = Entity(parent=oxygen_bar, model='quad', color=color.white, scale=(1, 1), origin=(-0.5, 0), position=(-0.5, 0))
+    altitude_label = Text(text="ALTITUDE: 0", position=(-0.75, 0.45))
+    distance_label = Text(text="DISTANCE: 0", position=(-0.75, 0.4))
+    fuel_label = Text(text="FUEL: 100%", position=(-0.75, 0.35))
+    speed_label = Text(text="SPEED: 0 m/s", position=(-0.75, 0.3))
+    current_stage_label = Text(text="STAGE: 1", position=(-0.75, 0.25))  # Assuming you start with Stage 1
+
+    atmospheric_layer_label = Text(text="LAYER: Troposphere", position=(-0.75, 0.2))
+    mass_label = Text(text="MASS: 0 kg", position=(-0.75, 0.15))  # Adjust the position as needed
+
 
 initialize_ui()
 
@@ -292,11 +431,15 @@ def setup_physics():
     
     earth_position = [0, 0, 0]
     earth_shape = p.createCollisionShape(p.GEOM_SPHERE, radius=earth_radius_game)
-    earth_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=earth_shape, basePosition=earth_position)
+    earth_body = p.createMultiBody(baseMass=0, baseCollisionShapeIndex=earth_shape, 
+                                   basePosition=earth_position)
     
+    rocket_max_velocity = 7500  # This is just an example value for Falcon 9. Adjust as needed
+
     rocket_position = [0, rocket_start_height, 0]
     rocket_shape = p.createCollisionShape(p.GEOM_BOX, halfExtents=[rocket_height_game/10, rocket_height_game/2, rocket_height_game/10])
     rocket_body = p.createMultiBody(baseMass=rocket_mass_game, baseCollisionShapeIndex=rocket_shape, basePosition=rocket_position)
+
 
 setup_physics()
 
@@ -334,6 +477,5 @@ def update():
         current_color = thermosphere_color
     
     window.color = current_color
-
 
 app.run()
