ecb2e5797c898002e29a834075727f55dec48a80
Victor <victor.lopez@king.com>
2023-11-06 16:37:59 +0100

    Harmonic wave

--- experiments/pressa.py
+++ experiments/pressa.py
@@ -27,7 +27,7 @@ final_distance = 0.001  # Final distance between the bars
 # Define time-dependent Hamiltonian
 def hamiltonian_t(t, args):
     d = initial_distance + (final_distance - initial_distance) * t / total_time
-    H_left_iron = args['left_iron_bar_strength'] * (a.dag() * a) / d # Swap the operators here
+    H_left_iron = args['left_iron_bar_strength'] * (a.dag() + a) / d # Swap the operators here
     H_right_iron = args['right_iron_bar_strength'] * (a.dag() + a) / d # Swap the operators here
     return H_field + H_left_iron + H_right_iron
 
@@ -36,14 +36,14 @@ emf_left_values = []
 emf_right_values = []
 
 # Set up the arguments for the Hamiltonian
-hamiltonian_args = {'left_iron_bar_strength': 1.0, 'right_iron_bar_strength': 1.0}
+hamiltonian_args = {'left_iron_bar_strength': 1.0, 'right_iron_bar_strength': 2.0}
 
 # Use sesolve to calculate the time-dependent results
-result = sesolve(hamiltonian_t, psi0, tlist=times, e_ops=[a.dag() * a, a.dag() + a], args=(hamiltonian_args)) # Swap the operators here
+result = sesolve(hamiltonian_t, psi0, tlist=times, e_ops=[a.dag() + a, a.dag() + a], args=(hamiltonian_args)) # Swap the operators here
 
 # Calculate EMF values
 emf_left_values = np.real(result.expect[0])
-emf_right_values = np.real(result.expect[1])
+emf_right_values = np.real(-result.expect[1])
 
 # Create separate plots for the left and right iron bars
 plt.figure(1)
