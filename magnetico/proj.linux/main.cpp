c83ac40969a3913738e314060af12dc0488871f9
Victor <victor.lopez@king.com>
2023-11-26 23:25:17 +0100

    First build

--- magnetico/proj.linux/main.cpp
+++ magnetico/proj.linux/main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <chrono>
 #include <memory>
+#include <filesystem>
 #include "components/Magnos.hpp"
 #include "components/EVEngine.hpp"
 #include "components/Laser.hpp"
@@ -10,7 +11,7 @@ int main(int argc, char** argv)
 {
     std::shared_ptr<EVEngine> engine = std::make_shared<EVEngine>();
     engine->setPosition3D(msr::airlib::Vector3r(0, 0, 0));
-    engine->init(std::filesystem::current_path().string() + "/");
+    engine->init("./");
 
     while (true)
     {
